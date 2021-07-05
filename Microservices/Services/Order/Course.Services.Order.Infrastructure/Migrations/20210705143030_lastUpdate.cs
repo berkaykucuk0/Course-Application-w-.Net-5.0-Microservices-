547554b800476f63de6da9365e16684b0401baea
Berkay Küçük <berkay.kucuk@ogr.sakarya.edu.tr>
2021-07-05 18:53:35 +0300

    Docker Updates
    Docker Updates

---
+++ Microservices/Services/Order/Course.Services.Order.Infrastructure/Migrations/20210705143030_lastUpdate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Course.Services.Order.Infrastructure.Migrations
+{
+    public partial class lastUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
